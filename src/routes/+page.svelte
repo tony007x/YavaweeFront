42d6c14569fcad55ca7a114afb96c0dca3428548
tony007x <aekanan2004@gmail.com>
2024-08-07 19:21:22 +0700

    setup-tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+const click = () =>{
+    alert("Hi")
+}
+</script>
+
+
+<div class="flex w-full h-screen justify-center">
+    <button on:click={click} >Click API</button>
+</div>
